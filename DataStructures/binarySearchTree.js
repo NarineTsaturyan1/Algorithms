49beff6d6a785c858e8cc1d930fb19f5bb77fafa
Narine Tsaturyan <narine_tsaturyan@epam.com>
2024-03-30 20:45:39 +0400

    Added predecessor and successor methods to BST.

--- DataStructures/binarySearchTree.js
+++ DataStructures/binarySearchTree.js
@@ -12,36 +12,32 @@ class BST {
   }
 
   insert(data) {
-    const node = this.root;
-    if(node === null) {
-      this.root = new Node(data);
-      return;
-    }
-    else {
-      const Search = function(node) {
-        if(data < node.data) {
-          if(node.left === null) {
-            node.left = new Node(data);
-            return
-          } else if(node.left !== null) {
-            return Search(node.left);
-          } else if(data > node.data) {
-            if(node.right === null) {
-              node.right = new Node(data);
-              return;
-            } else if(node.right !== null) {
-              return Search(node.right);
-            }
-          } else return null;
+    const newNode = new Node(data);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (currentNode) {
+        if (data < currentNode.data) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return;
+          }
+          currentNode = currentNode.right;
         }
       }
-      return Search(node);
     }
   }
 
   findMin() {
     let current = this.root;
-    while(current.left !== null) {
+    while (current.left !== null) {
       current = current.left;
     }
     return current.data;
@@ -49,17 +45,17 @@ class BST {
 
   findMax() {
     let current = this.root;
-    while(current.right !== null) {
-      current = current.roght;
+    while (current.right !== null) {
+      current = current.right;
     }
     return current.data;
   }
 
   find(data) {
     let current = this.root;
-    while(current.data !== data) {
-      if(current === null) return null;
-      if(data < current.data) {
+    while (current.data !== data) {
+      if (current === null) return null;
+      if (data < current.data) {
         current = current.left;
       } else {
         current = current.right;
@@ -69,31 +65,31 @@ class BST {
 
   isPresent(data) {
     let current = this.root;
-    while(current) {
-      if(data === current.data) return true;
-      if(data < current.data) current = current.left;
+    while (current) {
+      if (data === current.data) return true;
+      if (data < current.data) current = current.left;
       else current = current.right;
-      return false; 
+      return false;
     }
   }
 
   remove(data) {
-    const removeNode = function(node, data) {
-      if(node == null) return null;
-      if(data == node.data) {
+    const removeNode = function (node, data) {
+      if (node == null) return null;
+      if (data == node.data) {
         // no child
-        if(node.left == null && node.right == null) return null;
+        if (node.left == null && node.right == null) return null;
         // no left child
-        if(node.left == null) return node.right;
+        if (node.left == null) return node.right;
         // no right child
-        if(node.right == null) return node.left;
+        if (node.right == null) return node.left;
         // both children is there
         let tmp = node.right;
-        while(tmp.left !== null) tmp = tmp.left;
+        while (tmp.left !== null) tmp = tmp.left;
         node.data = tmp.data;
         node.right = removeNode(node.right, tmp.right);
         return node;
-      } else if(data < node.data) {
+      } else if (data < node.data) {
         node.left = removeNode(node.left, data);
         return node;
       } else {
@@ -109,8 +105,8 @@ class BST {
       return null;
     } else {
       var result = new Array();
-      function traversePreOrder(node) {     
-        result.push(node.data);  
+      function traversePreOrder(node) {
+        result.push(node.data);
         node.left && traversePreOrder(node.left);
 
         node.right && traversePreOrder(node.right);
@@ -125,7 +121,7 @@ class BST {
       return null;
     } else {
       var result = new Array();
-      function traverseInOrder(node) {       
+      function traverseInOrder(node) {
         node.left && traverseInOrder(node.left);
         result.push(node.data);
         node.right && traverseInOrder(node.right);
@@ -140,7 +136,7 @@ class BST {
       return null;
     } else {
       var result = new Array();
-      function traversePostOrder(node) {       
+      function traversePostOrder(node) {
         node.left && traversePostOrder(node.left);
         node.right && traversePostOrder(node.right);
         result.push(node.data);
@@ -152,30 +148,55 @@ class BST {
 
   findMinHeight(node = this.root) {
     if (node == null) {
-        return -1;
+      return -1;
     };
     let left = this.findMinHeight(node.left);
     let right = this.findMinHeight(node.right);
     if (left < right) {
-        return left + 1;
+      return left + 1;
     } else {
-        return right + 1;
+      return right + 1;
     };
   }
 
   findMaxHeight(node = this.root) {
     if (node == null) {
-        return -1;
+      return -1;
     };
     let left = this.findMaxHeight(node.left);
     let right = this.findMaxHeight(node.right);
-    if (left > right) {
-        return left + 1;
-    } else {
-        return right + 1;
-    };
+    return (Math.max(left, right) + 1);
   }
+
   isBalanced() {
     return (this.findMinHeight() >= this.findMaxHeight() - 1);
+  }
+
+  findPredecessor(root, key) {
+    let pred = null;
+    while (root !== null) {
+      if (key > root.data) {
+        pred = root;
+        root = root.right;
+      } else {
+        root = root.left;
+      }
+    }
+    if (pred == null) return null;
+    return pred.data;
+  }
+
+  findSuccessor(root, key) {
+    let suc = null
+    while (root !== null) {
+      if (key < root.data) {
+        suc = root;
+        root = root.left;
+      } else {
+        root = root.right;
+      }
+    }
+    if (suc == null) return null;
+    return suc.data;
   }
 }
